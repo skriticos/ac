6b40e69d885e88614979e7488efaf50f75855b7d
skriticos <seth.kriticos@googlemail.com>
2009-04-14 17:53:27 +0200

    myanimelist.py interface update
    Replaced the sync_mal function with the anime_data class, which also
    handles the local data file. It has a fetch and sync method and a
    database attribute. This makes it easy to sync the local file, app data
    and remote data at the same time. This approach is also much more readable.

--- src/AnimeCollector/myanimelist.py
+++ src/AnimeCollector/myanimelist.py
@@ -4,7 +4,9 @@
 # See COPYING for details
 
 from cookielib import LWPCookieJar
+import cPickle
 from datetime import date
+from os import path
 from urllib import urlencode
 from urllib2 import \
 		Request, urlopen, \
@@ -18,68 +20,85 @@ from data import \
 
 
 # ToDo: debug _filter_sync_changes and _push_update
-# ToDo: make sync_mal a class and add data storage/loading to it
 
 
-# setup cookie handler
-cookiejar = LWPCookieJar()
-opener = build_opener(HTTPCookieProcessor(cookiejar))
-install_opener(opener)
+class anime_data(object):
+	"""
+	Anime data module. Reads and writes local anime data to disk, fetches and
+	syncs with MyanimeList server.
 
-# set connection paramaters
-setdefaulttimeout(60)
-
-
-def sync_mal(username, password, ac_local_anime_dict={}):
-	""" MyAnimeList fetch and syncronization function interface.
-
-	This is the module interface function to performe fetching and
-	syncronization with the MyAnimeList server. If the ac_local_anime_dict
-	argument is obmitted, it just performs a fetch and returns the remote
-	database. If the ac_local_anime_dict argument dictionary is passed, it
-	also checks for differences and pushes local updates.
-
-	USAGE
-	Only fetch list: fetched_list = sync_mal('user', 'passwd')
-	Sync with local list:
-		(synced, updates, deletes) = sync_mal('user', 'passwd', local_list)
-
-	INPUT
-	username -- username for server authentication and list fetching
-	password -- password for server authentication
-	ac_local_anime_dict -- local anime dictionary that is used for server
-		syncronization
-
-	OUTPUT
-	False -- on login failure (wrong credentials)
-	ac_remote_anime_dict -- if only fetching is performed. This contains the
-		remote anime data.
-	(synced_anime_dict, remote_updates, deleted_entry_keys) -- if full sync is
-		performed. synced_anime_dict is the syncronized version of the local
-		and remote anime directories. remote_updates are the anime datasets
-		that were changed on the server. deleted_entry_keys are the keys of the
-		deleted datasets on the	server
+	username: login username
+	password: login password
+	db_path: path to database
+	db: local anime database that is a nested dict and has ASCII-fied series
+		titles as keys and and fields form mal_data_schema as dict data.
 	"""
 
-	if _login(username, password):
-		ac_remote_anime_dict = _fetch_list(username)
-		if ac_local_anime_dict:
+	def __init__(self, username='', password='', initsync=False):
+		"""
+		Setup credentials, read local data and setup network connection
+		environment. Optionally sync with MAL on startup.
+		"""
+
+		self.db_path = path.join(path.expanduser("~"), ".animecollector.dat")
+
+		self.username = username
+		self.password = password
+
+		# setup cookie handler
+		opener = build_opener(HTTPCookieProcessor(LWPCookieJar()))
+		install_opener(opener)
+
+		# set connection paramaters
+		setdefaulttimeout(60)
+
+		# initialize local data (read file if existent)
+		self.db = {}
+		if path.isfile(self.db_path):
+			db_handle = open(self.db_path, 'rb')
+			self.db = cPickle.load(db_handle)
+			db_handle.close()
+
+		if initsync:
+			self.sync()
+
+	def fetch(self):
+		"""
+		Only fetch anime data from MyAnimeList server (overwrites local data,
+		if existent). Useful for initializing and resetting local database.
+
+		Returns a copy of the fetched database on success, None on failure.
+		"""
+		if _login(self.username, self.password):
+			self.db = _fetch_list(self.username)
+			db_handle = open(self.db_path, 'wb')
+			cPickle.dump(self.db, db_handle)
+			db_handle.close()
+			return self.db
+
+	def sync(self):
+		"""
+		Syncronize local anime database with the MyAnimeList server.
+		(fetch -> compare -> push -> update local)
+
+		Returs nested dict of remote updates with ASCII-fied series titles as
+		keys and a list of keys that got deleted on the MyAnimeList server.
+		"""
+		if _login(self.username, self.password):
+			remote_db = _fetch_list(self.username)
 			(remote_updates, local_updates, deleted_entry_keys) = \
-				_filter_sync_changes(ac_remote_anime_dict, ac_local_anime_dict)
+				_filter_sync_changes(remote_db, self.db)
 			_push_list(local_updates)
+
 			# update local anime list with changes
 			for key in deleted_entry_keys:
-				del ac_local_anime_dict[deleted_entry_keys]
-			synced_anime_dict = ac_local_anime_dict
+				del self.db[deleted_entry_keys]
 			for key, value in remote_updates.items():
-				synced_anime_dict[key] = value
-			# complete sync cycle done
-			return (synced_anime_dict, remote_updates, deleted_entry_keys)
-		# fetching only
-		return ac_remote_anime_dict
-	else:
-		# login failed
-		return False
+				self.db[key] = value
+			db_handle = open(self.db_path, 'wb')
+			cPickle.dump(self.db, db_handle)
+			db_handle.close()
+			return (remote_updates, deleted_entry_keys)
 
 
 def _login(username, password):
@@ -269,265 +288,3 @@ def _push_list(local_updates):
 				print 'Error code: ', e.code
 			return False
 	return True
-
-
-
-
-#
-# """
-# 	This is a myanimelist data source module for animecollector.
-# """
-#
-# import Queue
-# from twisted.web import client
-# from twisted.internet import reactor
-# from xml.dom import minidom
-# import time
-# from datetime import date
-# import urllib
-#
-# class data_source:
-#
-# 	data = {}
-# 	dataList = []  # All of the data and the types of data provided by this source.
-# 	status = Queue.Queue()  # Current status.
-# 	identified = False  # True if logged in, False if not.
-#
-# 	_username = None
-# 	_password = None
-# 	_cookies = None
-#
-# 	def __init__(self):
-# 		""" Initialises all of the data for this source. """
-#
-# 		self.dataList = [
-# 			("series_animedb_id", int),
-# 			("series_title", unicode),
-# 			("series_synonyms", unicode),
-# 			("series_type", int),
-# 			("series_episodes", int),
-# 			("series_status", int),
-# 			("series_start", date),
-# 			("series_end", date),
-# 			("series_image", str),
-# 			("my_id", int),
-# 			("my_watched_episodes", int),
-# 			("my_start_date", date),
-# 			("my_finish_date", date),
-# 			("my_score", int),
-# 			("my_status", int),
-# 			("my_rewatching", int),
-# 			("my_rewatching_ep", int),
-# 			]
-#
-# 	def identify(self, username=None, password=None):
-# 		""" Authenticates with the mal server. """
-#
-# 		# move this stuff to the constructor
-# 		if username:
-# 			self._username = username
-# 		if password:
-# 			self._password = password
-#
-# 		# move this to the constant section
-# 		uri = "http://myanimelist.net/login.php"
-#
-# 		logininfo = urllib.urlencode({'username': self._username,
-# 				'password': self._password, 'cookie': 1, 'sublogin': "Login"})
-#
-# 		request = client.HTTPClientFactory(uri, method="POST", postdata=
-# 				logininfo, agent="animecollector", cookies=self._cookies,
-# 				headers={'Content-Type': 'application/x-www-form-urlencoded'})
-# 		(scheme, host, port, path) = client._parse(uri)
-#
-# 		reactor.connectTCP(host, port, request)
-# 		# request.deferred.addCallback(self._deffer_identify, request)
-# 		# request.deferred.addErrback(self._deffer_identify, None)
-#
-# 		if request.count('<div class="goodresult">'):
-# 			self._cookies = data.cookies
-# 			self.identified = True
-# 		else:
-# 			self.identified = False
-# 		self.status.put((data, success, otherdata))
-#
-# 		# XXX: this thing must get a return value telling if the login was
-# 		#	  successfull
-# 		# Note: reseach how well this deferred thing works togeher with gtk
-# 		# callbacks. Do some tracing and clean the module.
-#
-# 	def _deffer_identify(self, request, data):
-# 		""" Checks, if the authentication with the mal server was successfull.
-# 		(in a deffered way over a few sideways.)
-# 		"""
-#
-# 		if data:
-# 			if request.count('<div class="goodresult">'):
-# 				self._cookies = data.cookies
-# 				self.identified = True
-# 			else:
-# 				self.identified = False
-# 		else:
-# 			self.identified = False
-# 		self._add_to_queue("identify", self.identified)
-#
-# 	def unidentify(self):
-# 		self._cookies = None
-#
-# 	def update(self):
-# 		""" Updates all of the data from the source into this class. """
-#
-# 		if self._username:
-# 			uri = \
-# 				"http://myanimelist.net/malappinfo.php?status=all&u=" + str(self._username)
-# 			deffer = self._request(uri, method="GET")
-# 			deffer.addBoth(self._deffer_update)
-# 		else:
-# 			self._add_to_queue("update", False)
-#
-# 	def commit(self, ident, edits, olddata, data):
-# 		""" Commits edits made to the data to the site. """
-#
-# 		''' ident ('my_animedb_id') (Int), edits ([ ( name, value ) ]) (List) '''
-#
-# 		if not self.identified:
-# 			self._add_to_queue("commit", False)
-# 			return 0
-#
-# 		anime = olddata[ident]
-# 		last_watched_eps = anime["episodes_watched"]
-# 		last_status = anime["watching_status"]
-#
-# 		anime = data[ident]
-#
-# 		postdat = urllib.urlencode({
-# 			'series_id': anime["mal_my_id"],
-# 			'anime_db_series_id': anime["mal_id"],
-# 			"series_title": anime["mal_id"],
-# 			'aeps': anime["episodes_total"],
-# 			'astatus': anime["airing_status"],
-# 			'close_on_update': 'true',
-# 			'status': anime["watching_status"],
-# 			'rewatch_ep': anime["rewatching_episode"],
-# 			'last_status': last_status,
-# 			'completed_eps': anime["episodes_watched"],
-# 			'last_completed_eps': last_watched_eps,
-# 			"score": anime["score"],
-# 			'startMonth': self._parse_date(anime["start_airing"],
-# 					"month"),
-# 			'startDay': self._parse_date(anime["start_airing"], "day"),
-# 			'startYeah': self._parse_date(anime["start_airing"], "year"),
-# 			'submitIt': 2,
-# 			})
-# 		uri = \
-# 			"http://myanimelist.net/panel.php?keepThis=true&go=edit&id=" + \
-# 			str(anime["mal_my_id"]) + "&hidenav=true"
-# 		headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-# 		data = self._request(uri, method="POST", postdata=postdat,
-# 							 headers=headers)
-# 		data.addBoth(self._deffer_commit)
-#
-# 	def return_as_dic(self):
-# 		return self.data
-#
-# 	def _parse_date(self, thedate, back):
-# 		date = self._sanitise_date(thedate)
-# 		if date:
-# 			if back is "month":
-# 				return date.month
-# 			elif back is "year":
-# 				return date.year
-# 			elif back is "day":
-# 				return date.day
-# 			else:
-# 				raise ValueError
-# 		else:
-# 			return 0
-#
-# 	def _sanitise_date(self, thedate):
-# 		if type(thedate) is date:
-# 			if thedate:
-# 				return thedate
-# 			else:
-# 				return None
-# 		if type(thedate) is str:
-# 			if not thedate == "0000-00-00":
-# 				year = thedate.split("-")[0]
-# 				month = thedate.split("-")[1]
-# 				day = thedate.split("-")[2]
-# 				if not year == "0000":
-# 					if not month == "00":
-# 						if not day == "00":
-# 							return date.fromtimestamp(time.mktime(time.strptime(thedate,
-# 																	"%Y-%m-%d")))
-# 						else:
-# 							return None
-# 					else:
-# 						return None
-# 				else:
-# 					return None
-# 			else:
-# 				return None
-#
-# 	def _request(self, uri, method="POST", postdata=None, headers=None):
-# 		return client.getPage(uri, method=method, cookies=self._cookies,
-# 							  postdata=postdata, agent="animecollector",
-# 							  headers=headers)
-#
-# 	def _deffer_update(self, data):
-# 		try:
-# 			raw = minidom.parseString(data)
-# 		except:
-# 			self._add_to_queue("update", False)
-# 			return 0
-# 		self._parse_update(raw)
-#
-# 	def _deffer_commit(self, data):
-# 		if data.count('<div class="goodresult">'):
-# 			self._add_to_queue("commit", True)
-# 		else:
-# 			self._add_to_queue("commit", False)
-#
-# 	def _parse_update(self, raw):
-# 		if raw:
-# 			for anime in raw.getElementsByTagName("anime"):
-# 				this = {}
-# 				for (name, dtype) in self.dataList:
-# 					this[name] = self._get_from_xml(anime, (name, dtype))
-# 				(self.data)[self._get_from_xml(anime, (self.dataList)[0])] = \
-# 					this
-# 			self._add_to_queue("update", True)
-# 		else:
-# 			self._add_to_queue("update", False)
-#
-# 	def _get_from_xml(self, tree, (node, dtype)):
-# 		endnode = tree.getElementsByTagName(node)
-# 		if endnode:
-# 			if endnode[0]:
-# 				if endnode[0].firstChild:
-# 					if endnode[0].firstChild.nodeValue:
-# 						if dtype is int:
-# 							return int(endnode[0].firstChild.nodeValue)
-# 						elif dtype is date:
-# 							retdate = self._sanitise_date(endnode[0].firstChild.nodeValue)
-# 							if retdate:
-# 								return retdate
-# 							else:
-# 								return None
-# 						elif dtype is str:
-# 							return str(endnode[0].firstChild.nodeValue)
-# 						elif dtype is unicode:
-# 							return unicode(endnode[0].firstChild.nodeValue)
-# 					else:
-# 						return None
-# 				else:
-# 					return None
-# 			else:
-# 				return None
-# 		else:
-# 			return None
-#
-# 	def _add_to_queue(self, data, success, otherdata=None):
-# 		# XXX: seems to magically signal that the login was successfull.
-# 		self.status.put((data, success, otherdata))
-#
