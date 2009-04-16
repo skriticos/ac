00c6879f617d1a0e457243c5f98fe91ebf87a76a
Necrotex <ich@necrotex.de>
2009-04-16 16:32:28 +0200

    New File: recognizinig.py Desc: Recognizinig engine

--- src/AnimeCollector/recognizinig.py
+++ src/AnimeCollector/recognizinig.py
@@ -15,19 +15,30 @@ from data import mal_data_schema
 
 
 class filename_processor:
-     """Prepare the filename for the matching engine. Can later be used for the curent playing status"""
+     """Prepare the filename for the matching engine. Can later be used for the current playing status and the file rename engine"""
      def __init__(self, filename):
-          self.filename = filename
-          self.anime = str
-          self.episode = int
+          self.filename = filenname
 
      def __filter(self):
           """Get rid of hashtags, subgroup, codec and such"""
-          pass
+         
+         # First version of the RegEx. matches all charactes between "[" and "]"
+          reg = re.compile("([[a-zA-Z0-9]*])")
+          anime_raw = reg.sub("", self.filename)
+         
+         # get rid of underscores
+          anime_raw = replace("_"," ",anime_raw)
+         
+         # get rid of the file extension, currently just .mkv, .mp4 and .avi
+          anime_raw = re.sub("^.+\.((mkv)|(mp4)|(avi))$","",anime_raw)
+          
+          return trim(anime_raw)
 
      def _get_name(self):
           """getting and returning anime name"""
-          pass
+         
+         animeName = re.spilit(self.__filter())
+         
 
      def _get_episode(self):
           """getting and returning anime episode"""
