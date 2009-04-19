6753a17400603201cc58b2e3b954cf6ebe9b07f5
skriticos <seth.kriticos@googlemail.com>
2009-04-19 20:54:01 +0200

    Implementing GTK tree view.

--- src/AnimeCollector/gtkmain.py
+++ src/AnimeCollector/gtkmain.py
@@ -17,7 +17,8 @@ This module also has important global class instances:
 """
 
 import gtk
-from gtk import glade
+from gtk import glade, TreeView, ListStore
+import gobject
 from os import path
 from webbrowser import open as webopen
 
@@ -69,6 +70,65 @@ class widget_wrapper(object):
 	def __getitem__(self, key): return self.widgets.get_widget(key)
 
 
+def populate_tree_view(widgets, anime_data):
+	""" Populate the GTK TreeView interface widget from the anime_data.
+
+	The TreeView uses the gtk.ListStore interface model to display the
+	anime lists. It populates all 4 tabs.
+
+	Input:
+	  widgets -- widget wrapper reference to access the TreeView
+      anime_data -- anime_data instance (myanimelist module)
+	"""
+
+	# note: works on a display basis
+	# needs to get data fed
+
+	liststore = ListStore(str, str, int, int, int)
+	
+	column_schema = []
+
+	column_schema.append((gtk.TreeViewColumn('Title'), "text"))
+	column_schema.append((gtk.TreeViewColumn('Status'), "text"))
+	column_schema.append((gtk.TreeViewColumn('Score'), "spin"))
+	column_schema.append((gtk.TreeViewColumn('Episodes'), "spin"))
+	column_schema.append((gtk.TreeViewColumn('Progress'), "progress"))
+
+
+	value_index = 0
+	for column, type in column_schema:
+		if type == 'text':
+			# set it to expand
+			cell = gtk.CellRendererText()
+			column.pack_start(cell, True)
+			column.add_attribute(cell, 'text', value_index)
+		elif type == 'spin':
+			# set the right values from data
+			cell = gtk.CellRendererSpin()
+			adjustment = gtk.Adjustment(0, 0, 10, 1)
+			cell.set_property("adjustment", adjustment)
+			cell.set_property("editable", True)
+			column.pack_start(cell, True)
+			column.add_attribute(cell, 'text', value_index)
+		elif type == "progress":
+			cell = gtk.CellRendererProgress()
+			column.pack_start(cell, True)
+			column.add_attribute(cell, 'value', value_index)
+
+		widgets['treeview_watching'].append_column(column)
+		value_index += 1
+
+	# add data feeding
+	liststore.append(['title', 'status', 5, 5, 50])
+	liststore.append(['title', 'status', 5, 5, 50])
+	liststore.append(['title', 'status', 5, 5, 50])
+	liststore.append(['title', 'status', 5, 5, 50])
+	liststore.append(['title', 'status', 5, 5, 50])
+	liststore.append(['uoastuhsotanhu soahsutoahsuhaostitle', 'status', 5, 5, 50])
+
+	widgets['treeview_watching'].set_model(liststore)
+
+
 def switch_main_visible(event):
 	"""
 	This is the callback for the trayicon, which swiches the interface
@@ -97,6 +157,8 @@ def init_gui():
 	trayicon.set_from_file(path.join(ac_package_path, 'data', 'ac.ico'))
 	trayicon.connect('activate', switch_main_visible)
 
+	populate_tree_view(widgets, None)
+
 	# ready.. steady.. go!
 	gtk.main()
 
@@ -107,5 +169,8 @@ def run():
 	"""
 	global config
 	config = ac_config()
+	global mal_anime_data
+	mal_anime_data = \
+			anime_data(config.mal['username'], config.mal['password'])
 	init_gui()
 
