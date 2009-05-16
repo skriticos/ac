21bec23763208f540c84d1a949c0629948a883e1
Wile <towb@celvina.de>
2009-05-16 13:03:28 +0200

    More working tests!

--- src/AniChou/malcom.py
+++ src/AniChou/malcom.py
@@ -26,6 +26,8 @@ class Request(object):
     def execute(self, opener):
         """
         Takes OpenDirector.
+        
+        This does all the work and can take a while.
         """
         self.response = opener.open(self.request)
 
@@ -34,6 +36,8 @@ class Request(object):
         Yield content.
         
         Only after execution!
+        
+        This should be fast.
         """
         yield self.response.read()
 
@@ -54,7 +58,7 @@ class MAL(Request):
 
     def execute(self, opener):
         Request.execute(self, opener)
-        # Now set.
+        # Super set attribute.
         self.content = self.response.read()
         # Translate 200 OK with human-readable error message.
         if self.content.strip() == "Invalid username":
@@ -99,41 +103,65 @@ class Panel(MAL):
             )
         MAL.__init__(self, url, query, {}, data)
 
-class MALErrorProcessor(urllib2.BaseHandler):
-    """
-    Translate the site's human-readable error messages into proper HTTP.
-    """
+class List(MAL):
+    def __init__(self, username, typ = "anime"):
+        """
+        Type may be anime or manga.
+        """
+        url = "http://myanimelist.net/malappinfo.php"
+        query = dict(
+            u = username,
+            status = "all"
+            )
+        if typ == "manga":
+            query["type"] = typ
+        MAL.__init__(self, url, query, {})
 
-    def http_response(self, request, response):
-        code, msg, hdrs = response.code, response.msg, response.info()
-        if code != 200:
-            # We only handle non-obvious errors.
-            return response
-        html = response.read().strip()
-        # People expect that. Only it doesn't work, breaking the whole class.
-        # There is a seek_wrapper in
-        # http://codespeak.net/svn/wwwsearch/ClientCookie/trunk/ClientCookie/_Util.py
-#       response.seek(0)
-        if html == "Invalid username":
-            # Returned by appInfo.
-            return self.parent.error(
-                'http', request, response, 404, msg, hdrs)
-        match = re.search(
-            r'<div.+?class\s*=\s*["\']?badresult.+?>(.+?)</div>', html)
-        if not match:
-            # All is right.
-            return response
-        result = match.group(1)
-        for error in ("Could not find that username", "Invalid password",
-            "You must first login"):
-            # Returned by login and panel, respectively.
-            if error in result:
-                # Let the others guess whether to re-try.
-                return self.parent.error(
-                    'http', request, response, 401, msg, hdrs)
-        # We got *something* bad but don't dare put the message in the error.
-        return self.parent.error(
-            'http', request, response, 400, msg, hdrs)
+class Search(MAL):
+    def __init__(self, query, typ = "anime"):
+        typ = ("anime", "manga", "all").index(typ) + 1
+        url = "http://myanimelist.net/includes/masearch.inc.php"
+        query = dict(
+            s = query,
+            type = typ
+            )
+        head = dict(Referer = "http://myanimelist.net/addtolist.php")
+        MAL.__init__(self, url, query, head)
+
+class Add(MAL):
+    def __init__(self, series_animedb_id, status, completed_eps, score = 0):
+        """
+        Takes values from mal_anime_data_schema.
+        """
+        url = "http://myanimelist.net/includes/masearch.inc.php"
+        if not completed_eps:
+            # How the site does it.
+            completed_eps = ""
+        query = dict(
+            animeid = series_animedb_id,
+            status = status,
+            score = score,
+            eps = completed_eps
+            )
+        head = dict(Referer = "http://myanimelist.net/addtolist.php")
+        MAL.__init__(self, url, query, head)
+
+    def execute(self, opener):
+        MAL.execute(self, opener)
+        # Super set attribute.
+        if self.content.strip() != "maSuccessAdded":
+            raise URLError, "couldn't add"
+
+class Login(MAL):
+    def __init__(self, username, password):
+        url = "http://myanimelist.net/login.php"
+        data = dict(
+            username = username,
+            password = password,
+            sublogin = "Login",
+            cookie = 1
+            )
+        MAL.__init__(self, url, {}, {}, data)
 
 class Site(object):
     """
@@ -152,285 +180,3 @@ class Site(object):
         
     def login(self, username, password):
         pass
-
-class Remote(object):
-    """
-    A client for part of a server.
-    
-    Most methods use keyword arguments even for required information so that
-    subclasses can provide defaults.
-    """
-
-    def url(self, **kwargs):
-        """
-        Return a URL string.
-        
-        _url    A full URL with scheme and path. Query strings, if present,
-                are discarded. Required argument.
-        xyz     Everything else is used to build a query string.
-
-        Always returns self._url if set.
-        """
-        try:
-            return self._url
-        except AttributeError:
-            pass
-        # Raises KeyError.
-        tmpl = kwargs.pop("_url")
-    	# Make tuple mutable.
-    	tmpl = list(urlparse.urlparse(tmpl))
-    	# New parameters.
-    	tmpl[4] = urllib.urlencode(kwargs)
-    	# The modified template.
-    	return urlparse.urlunparse(tmpl)
-
-    def request(self, **kwargs):
-        """
-        Return a request object.
-        
-        _url    A full URL which may contain a query string.
-                It won't be touched. Defaults to self.url()
-        xyz     Everything else is used as HTTP headers.
-                Those are somewhat case-sensitive.
-                
-        Referer is recommended.
-
-        Always returns self._request if set.
-        """
-        try:
-            return self._request
-        except AttributeError:
-            pass
-        try:
-            tmpl = kwargs.pop("_url")
-        except KeyError:
-            tmpl = self.url()
-    	return urllib2.Request(tmpl, headers = kwargs)
-
-    def post(self, **kwargs):
-        """
-        Return a modified request object.
-        
-        _request    A request object complete with headers. Required.
-        xyz         Everything else is added as form-urlencoded data,
-                    turning the request from GET into POST.
-        """
-        # Raises KeyError.
-        req = kwargs.pop("_request")
-    	# add_data automatically sets content-type and turns req into POST.
-        req.add_data(urllib.urlencode(kwargs))
-        return req
-        
-    def response(self, opener, reget = False):
-        """
-        Execute request and return result.
-        
-        Use the given OpenDirector, which should contain a (logged in)
-        CookieProcessor, to execute self.request()
-        
-        Also stores the result and will return it immediatly in the future,
-        unless reget is True.
-        
-        Normally the response can only be read() once, as sockets don't know
-        seek(). To get around this we replace it with StringIO. This means
-        the whole body takes up memory.
-        """
-        if reget:
-            del self._response
-        try:
-            return self._response
-        except AttributeError:
-            pass
-        resp = opener.open(self.request())
-        # Construct replacement like in AbstractHTTPHandler.
-        self._response = urllib.addinfourl(
-            # We can use cStringIO because urllib2 doesn't return unicode(),
-            # except encoded. Right?
-            StringIO(resp.read()),
-            resp.headers,
-            resp.url
-            )
-        # Copy attributes. Violates encapsulation.
-        self._response.code = resp.code
-        self._response.msg = resp.msg
-        return self._response
-
-class MAL(Remote):
-    def request(self, **kwargs):
-        head = {"User-Agent": "AniChou"}
-        head.update(kwargs)
-        return Remote.request(self, **head)
-
-    def response(self, opener, reget = False):
-        resp = Remote.response(self, opener, reget)
-        # Code from MALErrorProcessor goes here.
-
-class List(MAL):
-    def __init__(self, username, typ = "anime"):
-        """
-        Type may be anime or manga.
-        """
-        kwargs = dict(u = username)
-        if typ == "manga":
-            kwargs["type"] = typ
-        self._url = self.url(**kwargs)
-
-    def url(self, **kwargs):
-        """
-        Return an appInfo URL.
-        
-        u       Username. Required.
-        status  Default 'all'.
-        type    May be 'manga'. Otherwise seems to list anime.
-        """
-        if not "u" in kwargs:
-            raise KeyError
-        defaults = dict(
-            _url = "http://myanimelist.net/malappinfo.php",
-            status = "all"
-            )
-        defaults.update(kwargs)
-        return MAL.url(self, **defaults)
-
-class Search(MAL):
-    def __init__(self, query, typ = "anime"):
-        typ = ("anime", "manga", "all").index(typ) + 1
-        self._url = self.url(s = query, type = typ)
-
-    def url(self, **kwargs):
-        """
-        Return search URL.
-        
-        s       Search term. Required.
-    	type    1 for anime only (default), 2 for manga only, 3 for both.
-        """
-        if not "s" in kwargs:
-            raise KeyError
-        defaults = dict(
-            _url = "http://myanimelist.net/includes/masearch.inc.php",
-            # Need not be string.
-            type = 1
-            )
-        defaults.update(kwargs)
-        return MAL.url(self, **defaults)
-
-    def request(self, **kwargs):
-        """
-        Return a modified request object.
-        
-        _url    Required argument.
-        Referer Default.
-        """
-        # How it is called on the site.
-        defaults = {"Referer": "http://myanimelist.net/addtolist.php"}
-        defaults.update(kwargs)
-        return MAL.request(self, **defaults)
-
-class Login(MAL):
-    def __init__(self, username, password):
-        self._request = self.post(username = username, password = password)
-        
-    def url(self, **kwargs):
-        """
-        Return a login URL.
-        """
-        defaults = dict(_url = "http://myanimelist.net/login.php")
-        defaults.update(kwargs)
-        return MAL.url(self, **defaults)
-
-    def post(self, **kwargs):
-        """
-        Return a request object.
-        
-        _request    Not required!
-        username    Required.
-        password    Required.
-        cookie      How to track the session. Should be 1 (default)
-        sublogin    The submit button used. Should be 'Login' (default)
-        """
-        if not "username" in kwargs or not "password" in kwargs:
-            raise KeyError
-        defaults = dict(
-            _request = self.request(_url = self.url()),
-            cookie = 1,
-            sublogin = "Login"
-            )
-        defaults.update(kwargs)
-        return MAL.post(self, **defaults)
-
-class Panel(MAL):
-    def __init__(self, anime):
-        """
-        Argument must conform to mal_anime_data_schema.
-        """
-        data = dict(
-            _request = self.request(_url = self.url(id = anime["my_id"]))
-            )
-        # Map post arguments to input fields.
-        schema = dict(
-            series_animedb_id = "series_animedb_id",
-            series_title = "series_animedb_id",
-            completed_eps = "my_watched_episodes",
-            status = "my_status",
-            score = "my_score"
-            )
-        for to, von in schema.iteritems():
-            data[to] = anime[von]
-        self._request = self.post(**data)
-        
-    def url(self, **kwargs):
-        """
-        Return a panel URL.
-        
-        keepThis    Should be string 'true' (default)
-        go          Which dialog to render. Irrelevant to us. Can be
-                    'edit' (default), 'add' or 'addmanga'.
-        id          User-relation id of a series. Required.
-        hidenav     Whether to render a header. Irrelevant. Defaults to 1.
-                    Probably also accepts string 'true'.
-        TB_iframe   Whatever. Usually true on the site,
-                    but we default to 'false'.
-        """
-        if not "id" in kwargs:
-            raise KeyError
-        defaults = dict(
-            _url = "http://myanimelist.net/panel.php",
-            keepThis = "true",
-            go = "edit",
-            hidenav = 1,
-            TB_iframe = "false"
-            )
-        defaults.update(kwargs)
-        return MAL.url(self, **defaults)
-
-    def post(self, **kwargs):
-        """
-        Return a request object.
-        
-        close_on_update Probably sends back JavaScript to remove the panel.
-                        Irrelevant, defaults to 'true'.
-        submitIt        Which submit button was used. Defaults to 2.
-
-        Arguments from the anime_data structure:
-        
-        series_animedb_id   Probably required, but we don't.
-        series_title        For some reason the site sets this to the same
-                            number as series_animedb_id, so if you don't pass
-                            it explicitly we do that as well.
-        completed_eps       Corresponds to my_watched_episodes.
-                            We accept that as well.
-        status              my_status. No shortcut yet.
-        score               my_score
-        """
-        defaults = dict(
-            submitIt = 2,
-            close_on_update = "true"
-            )
-        if "series_animedb_id" in kwargs:
-            defaults["series_title"] = kwargs["series_animedb_id"]
-        if "my_watched_episodes" in kwargs:
-            # Site doesn't know about our convenience.
-            defaults["completed_eps"] = kwargs.pop("my_watched_episodes")
-        # Overwrite explicit arguments.
-        defaults.update(kwargs)
-        return MAL.post(self, **defaults)
--- test/malcom.py
+++ test/malcom.py
@@ -1,65 +1,87 @@
-from __future__ import with_statement
-
 import unittest
 import urllib2
 import test.test_urllib2
 import os
 import sys
 import mimetools
+import gzip
 
-class TestRemote(unittest.TestCase):
+class TestRequest(unittest.TestCase):
     def setUp(self):
-        self.rem = AniChou.malcom.Remote()
-        self.req = self.rem.request(_url = "http://www.google.com")
+        self.req = AniChou.malcom.Request(
+            "http://www.google.com",
+            dict(q = "AniChou"),
+            dict(Referer = "http://myanimelist.net")
+            )
 
-    def testUrl(self):
-        self.assertRaises(KeyError, lambda: self.rem.url())
-        self.assertEqual("http://www.google.com",
-            self.rem.url(_url = "http://www.google.com"))
-        self.assertEqual("http://www.google.com?q=AniChou", self.rem.url(
-            _url = "http://www.google.com",
-            q = "AniChou"
-            ))
+    def testAttribute(self):
+        req = self.req.request
+        self.assertEqual("http://www.google.com?q=AniChou",
+            req.get_full_url())
+        self.assert_(req.has_header("Referer"))
 
-    def testRequest(self):
-        self.assertRaises(KeyError, lambda: self.rem.request())
-        self.assertEqual("http://www.google.com", self.req.get_full_url())
-
-    def testPost(self):
-        self.assertRaises(KeyError, lambda: self.rem.post())
-        self.req = self.rem.post(_request = self.req)
-        self.assert_(self.req.has_data())
-        self.assertEqual("POST", self.req.get_method())
+    def testIter(self):
+        self.assertRaises(AttributeError, lambda: list(self.req))
 
 class TestMAL(unittest.TestCase):
     def setUp(self):
-        self.rem = AniChou.malcom.MAL()
-        self.req = self.rem.request(_url = "http://www.google.com")
+        self.req = AniChou.malcom.MAL("http://myanimelist.net", {}, {})
 
     def testRequest(self):
         # Capitalizes.
-        self.assert_(self.req.has_header("User-agent"))
+        self.assert_(self.req.request.has_header("User-agent"))
 
 class TestList(unittest.TestCase):
     def setUp(self):
-        self.rem = AniChou.malcom.List(username = "Wile")
+        self.req = AniChou.malcom.List(username = "Wile")
 
     def testUrl(self):
         # Dictionaries are unordered.
-        self.assert_(self.rem.url() in (
+        self.assert_(self.req.request.get_full_url() in (
             "http://myanimelist.net/malappinfo.php?u=Wile&status=all",
             "http://myanimelist.net/malappinfo.php?status=all&u=Wile"
             ))
 
+class TestListParsing(unittest.TestCase):
+    def setUp(self):
+        self.director = urllib2.build_opener(
+            MockHTTPHandler("list.xml.gz"))
+        self.request = AniChou.malcom.List(username = "crono22")
+
+    def testOpen(self):
+        # Shouldn't raise.
+        self.request.execute(self.director)
+
 class TestErrorAppInfo(unittest.TestCase):
     def setUp(self):
         self.director = urllib2.build_opener(
-            MockHTTPHandler(TESTS[2]),
-            AniChou.malcom.MALErrorProcessor
-            )
+            MockHTTPHandler("invalid username.txt"))
+        self.request = AniChou.malcom.List(username = "nobody")
 
     def testOpen(self):
-        self.director.open(AniChou.malcom.List(username = "nobody").url())
+        self.assertRaises(AniChou.malcom.UsernameError,
+            lambda: self.request.execute(self.director))
+
+class TestErrorLogin(unittest.TestCase):
+    def setUp(self):
+        self.director = urllib2.build_opener(
+            MockHTTPHandler("bad username.txt"))
+        self.request = AniChou.malcom.Login(username = "Wile",
+            password = "helo")
+
+    def testOpen(self):
+        self.assertRaises(AniChou.malcom.UsernameError,
+            lambda: self.request.execute(self.director))
+
+class TestMissingLogin(unittest.TestCase):
+    def setUp(self):
+        self.director = urllib2.build_opener(
+            MockHTTPHandler("must first login.txt"))
+        self.request = AniChou.malcom.Add(0, 0, 0)
+
+    def testOpen(self):
+        self.assertRaises(AniChou.malcom.LoginError,
+            lambda: self.request.execute(self.director))
 
 class MockHTTPHandler(urllib2.HTTPHandler):
     """
@@ -67,19 +89,24 @@ class MockHTTPHandler(urllib2.HTTPHandler):
     """
 
     def __init__(self, path):
-        """
-        Accepts about any file system path format.
-        """
         # Get rid of tilde.
         path = os.path.expanduser(path)
-        # Definetly have it start in slash.
+        # Definitely have it start in slash.
         self.path = os.path.abspath(path)
 
     def http_open(self, req):
-        with open(self.path, "r") as f:
-            msg = mimetools.Message(f)
-            body = f.read()
-        return test.test_urllib2.MockResponse(200, body, msg, "",
+        if self.path[-3:] == ".gz":
+            f = gzip.open(self.path)
+        else:
+            f = open(self.path)
+        # Discard first line.
+        http = f.readline()
+        # Parse header.
+        msg = mimetools.Message(f)
+        # Retain content.
+        body = f.read()
+        f.close()
+        return test.test_urllib2.MockResponse(200, "OK", msg, body,
             req.get_full_url())
 
 if __name__ == '__main__':
@@ -87,10 +114,4 @@ if __name__ == '__main__':
     sys.path.append(os.path.abspath("../src"))
     # Now we can.
     import AniChou.malcom
-    # Constant.
-    TESTS = []
-    # HTTP dumps.
-    for f in ["bad username.txt", "must first login.txt",
-        "invalid username.txt"]:
-        TESTS.append(os.path.abspath(f))
     unittest.main()
